95576b0506caffb6b8f44892014b88d76d34172a
Carolina Vejar <c.vejar.calluan@gmail.com>
2023-01-05 13:23:49 -0300

    Clase 130 - Busqueda por Slug o UUID

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { validate as isUUID} from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -51,10 +52,12 @@ export class ProductsService {
     };
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
-    if (! product)
-      throw new NotFoundException
+  async findOne(term: string) {
+    let product : Product;
+    if(isUUID(term))
+    product = await this.productRepository.findOneBy({ id: term });
+    else
+    product = await this.productRepository.findOneBy({ slug: term });
     return product
   }
 
