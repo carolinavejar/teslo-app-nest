e761396db9186611631d842c2ed73aae2e73bf51
Carolina Vejar <c.vejar.calluan@gmail.com>
2023-01-05 14:10:37 -0300

    Clase 132 -Update TypeORM

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -69,8 +69,20 @@ export class ProductsService {
     return product
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id: id,
+      ... updateProductDto
+    });
+    if (!product )
+      throw new NotFoundException (`Product with id ${ id } not found`);
+    
+      try {
+        return await this.productRepository.save(product)
+      } catch (error) {
+        this.handleExeptions(error)
+      }
+      
   }
 
   async remove(id: string) {
