18d95c327a6c1aee7d773cf66c4d598088b0733f
Carolina Vejar <caro.vejarc@gmail.com>
2023-02-25 18:55:23 -0300

    Clase 210 - Emit desde el server

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,5 +1,6 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { NewMessageDTO } from './dtos/new-message.dto';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway( { cors: true } )
@@ -17,4 +18,28 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
     this.messagesWsService.removeClient(client.id);
     this.wss.emit(`clients-updated`, this.messagesWsService.getConnectedClients())
   }
+
+  @SubscribeMessage(`message-from-client`)
+  handleMesageFromClient(client: Socket, payload: NewMessageDTO) {
+
+    /* Emite unicamente al cliente
+    client.emit(`message-from-server`, {
+      fullName: `carito`,
+      message: payload.message || `no message`
+    }) */
+
+    /* Emite a todos menos al cliente inicial 
+    client.broadcast.emit(`message-from-server`, {
+      fullName: `carito`,
+      message: payload.message || `no message`
+    }) */
+
+    this.wss.emit(`message-from-server`, {
+      fullName: `carito`,
+      message: payload.message || `no message`
+    })
+    
+  }
+
+
 }
